b788cfd05319a8461cf5c381387abe9f95d8b236
tomasz <tomasz@com>
2025-06-10 00:19:58 +0200

    rysowanie labirytnu

--- Zadanie_2.py
+++ Zadanie_2.py
@@ -1,46 +1,154 @@
 import sys
-from PyQt6 import QtCore, QtGui, QtWidgets, uic
-from PyQt6.QtCore import Qt
+import random
+from PyQt6.QtWidgets import (
+    QApplication, QMainWindow, QWidget, QLabel,
+    QVBoxLayout, QHBoxLayout, QPushButton, QComboBox, QMessageBox
+)
+from PyQt6.QtGui import QPixmap, QPainter, QColor, QPen, QMouseEvent
+from PyQt6.QtCore import Qt, QTime, QPoint
+
+#mouse events:
+class MazeLabel(QLabel):
+    def __init__(self, game):
+        super().__init__(game)
+        self.game = game
+        self.setMouseTracking(True)
+
+    def mousePressEvent(self, event: QMouseEvent):
+        self.game.handle_press(event)
+
+    def mouseMoveEvent(self, event: QMouseEvent):
+        self.game.handle_move(event)
+
+    def mouseReleaseEvent(self, event: QMouseEvent):
+        self.game.handle_release(event)
 
 
-class MainWindow(QtWidgets.QMainWindow):
+class MazeGame(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("Gra: Przejdź Labirynt")
+        self.cols, self.rows = 10, 12
+        self.cell_size = 40
+        self.W, self.H = self.cols * self.cell_size, self.rows * self.cell_size
 
-        self.label = QtWidgets.QLabel()
-        canvas = QtGui.QPixmap(400, 300)
-        canvas.fill(Qt.GlobalColor.white)
-        self.label.setPixmap(canvas)
-        self.setCentralWidget(self.label)
-        self.draw_something()
+    # #=ściana, S=start, E=meta, spacja=ścieżka
+        maps = [
+            [
+                "##########",
+                "#S   #   #",
+                "# ## ##  #",
+                "#    #  ##",
+                "#### #   #",
+                "#   #### #",
+                "# ##     #",
+                "#    ##  #",
+                "## ###   #",
+                "#   #    #",
+                "#  ##  E #",
+                "##########",
+            ],
+            [
+                "##########",
+                "# S      #",
+                "# ## ### #",
+                "#    #   #",
+                "#### # ###",
+                "#    #   #",
+                "# ## ### #",
+                "# ## #   #",
+                "#   ###  #",
+                "# ###   E#",
+                "#        #",
+                "##########",
+            ],
+            [
+                "##########",
+                "# S #    #",
+                "#   ## # #",
+                "###    # #",
+                "#   #### #",
+                "# #      #",
+                "# # ######",
+                "# #      #",
+                "#   ##   #",
+                "# ###  # #",
+                "#    #  E#",
+                "##########",
+            ],
+        ]
 
-    def draw_something(self):
-        from random import randint
-        canvas = self.label.pixmap()
-        painter = QtGui.QPainter(canvas)
+    # Parsowanie stringa do siatki 
+        self.mazes = []
+        for idx, m in enumerate(maps, start=1):
+            grid, Spos, Epos = [], None, None
+            for r, row in enumerate(m):
+                row_cells = []
+                for c, ch in enumerate(row):
+                    if ch == '#':
+                        row_cells.append(1)
+                    else:
+                        row_cells.append(0)
+                        if ch == 'S':
+                            Spos = (c, r)
+                        elif ch == 'E':
+                            Epos = (c, r)
+                grid.append(row_cells)
+            self.mazes.append({
+                "name": f"Labirynt {idx}",
+                "grid": grid, "start": Spos, "end": Epos
+            })
+
+        self._init_ui()
+        self.new_game()  # losowy wybór  na starcie
+
+    def _init_ui(self):
+        central = QWidget()
+        self.setCentralWidget(central)
+        vbox = QVBoxLayout(central)
+
+        self.label = MazeLabel(self)
+        self.label.setFixedSize(self.W, self.H)
+        vbox.addWidget(self.label, alignment=Qt.AlignmentFlag.AlignCenter)
+
+    # Przycisk wybór labiryntu, przycisk Nowa Gra
+        hbox = QHBoxLayout()
+        vbox.addLayout(hbox)
+        self.maze_combo = QComboBox()
+        for m in self.mazes:
+            self.maze_combo.addItem(m["name"])
+        hbox.addWidget(self.maze_combo)
+
+        new_btn = QPushButton("Nowa Gra")
+        new_btn.clicked.connect(self.new_game)
+        hbox.addWidget(new_btn)
+
+    def new_game(self):
+        idx = self.maze_combo.currentIndex()
+        self.current = self.mazes[idx]
+        
+
+        self.base_pixmap = QPixmap(self.W, self.H)
+        self._draw_maze(self.base_pixmap, self.current["grid"],
+                        self.current["start"], self.current["end"])
+     
+        self.path_pixmap = QPixmap(self.base_pixmap)
+        self.label.setPixmap(self.path_pixmap)
 
        
-        pen = QtGui.QPen()
-        pen.setWidth(60)
-        pen.setColor(QtGui.QColor('green'))
-        painter.setPen(pen)
-        painter.drawLine(10, 10, 300, 200)  #rysowanie linii
-        painter.drawPoint(200, 150)     #rysowanie punktu
+        self.is_drawing = False
+        self.start_time = None
+        self.last_pt = None
 
-        for n in range(10000):
-            painter.drawPoint(
-                200+randint(-75, 100),  # x
-                150+randint(-100, 80)   # y
-                )
-        
+        QMessageBox.information(
+            self, "Witaj!",
+            "Kliknij i przytrzymaj lewy przycisk myszy w zielonym polu START, "
+            "następnie prowadź kursor do czerwonego pola META nie dotykając ścian i "
+            "nie puszczając przycisku. Powodzenia")
 
 
-        painter.end()
-        self.label.setPixmap(canvas)
-
-        
-
-app = QtWidgets.QApplication(sys.argv)
-window = MainWindow()
-window.show()
-app.exec()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MazeGame()
+    window.show()
+    sys.exit(app.exec())
